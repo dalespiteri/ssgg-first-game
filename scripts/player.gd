5a0c6e99afc670a690cf38e3ebe7cdf9695a0920
Dale Spiteri <dalespiteri@gmail.com>
2024-06-10 17:45:59 -0400

    add player size toggling and sound effect

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const JUMP_VELOCITY = -250.0
 
 var jumps_remaining = 2
 var current_direction = 1
+var current_size = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,8 +13,27 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var a_p = $AnimationPlayer
 @onready var sprite_2d = $Sprite2d
 @onready var coyote_time = $CoyoteTime
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var shrink = $Shrink
+@onready var grow = $Grow
 
 func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("toggle_size"):
+		var colShape = CapsuleShape2D.new()
+		colShape.radius = 2.0 if current_size == 1 else 4.0
+		colShape.height = 7.5 if current_size == 1 else 15.0
+		collision_shape_2d.set_shape(colShape)
+		collision_shape_2d.position = Vector2(0, -10) if current_size == 1 else Vector2(0, -8)
+		var tween = create_tween()
+		if current_size == 1:
+			shrink.play()
+			tween.tween_property(sprite_2d, "scale", Vector2(0.5, 0.5), 0.1)
+		else:
+			grow.play()
+			tween.tween_property(sprite_2d, "scale", Vector2(1, 1), 0.1)
+		tween.connect("finished", on_tween_finished)
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -51,6 +71,11 @@ func _physics_process(delta):
 		
 	update_animations(direction)
 	
+func on_tween_finished():
+	if current_size == 1:
+		current_size = 0
+	else:
+		current_size = 1
 
 func update_animations(direction: int):
 	if is_on_floor():
